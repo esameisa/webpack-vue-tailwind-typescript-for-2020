3ba0d6c87a669f39873725a0b1b56c8d97967a1f
esameisa <esameisa12345@gmail.com>
2020-01-19 01:37:25 +0200

    update home.vue

--- src/home.vue
+++ src/home.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="flex justify-center mt-12">
-    <div
-      class="flex items-center bg-green-500 text-white text-sm font-bold px-12 py-3"
-      role="alert"
-    >
-      <svg
-        class="fill-current w-4 h-4 mr-2"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-      >
-        <path
-          d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"
+    <div class="max-w-sm mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+      <div class="sm:flex sm:items-center px-6 py-4">
+        <img
+          class="block mx-auto sm:mx-0 sm:flex-shrink-0 h-16 sm:h-24 rounded-full"
+          src="https://randomuser.me/api/portraits/women/17.jpg"
+          alt="Woman's Face"
         />
-      </svg>
-      <p>{{ message }}</p>
+        <div class="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
+          <p class="text-xl leading-tight">Erin Lindford</p>
+          <p class="text-sm leading-tight text-gray-600">{{ message }}</p>
+          <div class="mt-4">
+            <button
+              class="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-xs font-semibold rounded-full px-4 py-1 leading-normal"
+            >Message</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
